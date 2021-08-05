43955a70052ac7955bf59d59e32dc53d5043184a
williamandres <williamaguilar@misena.edu.co>
2021-08-05 15:03:42 -0500

    nest controller task bee

--- src/nest/nest.controller.ts
+++ src/nest/nest.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { get } from 'http';
 
 @Controller('nest')
-export class NestController {}
+export class NestController {
+
+    @Get('/GET')
+    getnest(){
+        return[
+            {
+                nombre: 'get',
+            }
+        ]
+    }
+
+    @Get('/POST')
+    postnest(){
+        return[
+            {
+                nombre: 'post',
+            }
+        ]
+    }
+
+    @Get('/PUT')
+    putnest(){
+        return[
+            {
+                nombre: 'put',
+            }
+        ]
+    }
+
+    @Get('/DELETE')
+    deleteNest(){
+        return[
+            {
+                nombre: 'delete',
+            }
+        ]
+    }
+
+
+}
